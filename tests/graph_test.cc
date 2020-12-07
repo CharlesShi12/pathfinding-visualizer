2ce2cab2abcd2e5ced2eb902d10692e779f1a91d
CharlesShi46 <shi46@illinois.edu>
2020-12-07 16:40:43 -0600

    Update Graph tests and add more Graph tests

--- tests/graph_test.cc
+++ tests/graph_test.cc
@@ -15,33 +15,17 @@ TEST_CASE("Testing the Graph class") {
                                  {0, 0}};
 
     auto *board_graph = new Graph(board);
+    size_t dimension = board_graph->GetDimension();
+    vector<Graph::Node *> nodes = board_graph->GetNodes();
 
-    REQUIRE(board_graph->GetDimension() == 2);
-    REQUIRE(board_graph->GetBoard() == board);
+    vector<vector<int>> adjacent = {{2, 1}, {3, 0}, {3, 0}, {2, 1}};
 
-    vector<int> adjacent = {2, 1};
-    REQUIRE(board_graph->GetNodes().at(0)->row == 0);
-    REQUIRE(board_graph->GetNodes().at(0)->col == 0);
-    REQUIRE(board_graph->GetNodes().at(0)->adjacent == adjacent);
-    REQUIRE(board_graph->GetNodes().at(0)->distance == INFINITY);
-
-    adjacent = {3, 0};
-    REQUIRE(board_graph->GetNodes().at(1)->row == 0);
-    REQUIRE(board_graph->GetNodes().at(1)->col == 1);
-    REQUIRE(board_graph->GetNodes().at(1)->adjacent == adjacent);
-    REQUIRE(board_graph->GetNodes().at(1)->distance == INFINITY);
-
-    adjacent = {3, 0};
-    REQUIRE(board_graph->GetNodes().at(2)->row == 1);
-    REQUIRE(board_graph->GetNodes().at(2)->col == 0);
-    REQUIRE(board_graph->GetNodes().at(2)->adjacent == adjacent);
-    REQUIRE(board_graph->GetNodes().at(2)->distance == INFINITY);
-
-    adjacent = {2, 1};
-    REQUIRE(board_graph->GetNodes().at(3)->row == 1);
-    REQUIRE(board_graph->GetNodes().at(3)->col == 1);
-    REQUIRE(board_graph->GetNodes().at(3)->adjacent == adjacent);
-    REQUIRE(board_graph->GetNodes().at(3)->distance == INFINITY);
+    for (size_t i = 0; i < dimension; i++) {
+      REQUIRE(nodes[i]->row == (i / dimension));
+      REQUIRE(nodes[i]->col == (i % dimension));
+      REQUIRE(nodes[i]->distance == INFINITY);
+      REQUIRE(nodes[i]->adjacent == adjacent[i]);
+    }
   }
 
   SECTION("Testing the Constructor with a 3x3 board with no walls") {
@@ -50,63 +34,19 @@ TEST_CASE("Testing the Graph class") {
                                  {0, 0, 0}};
 
     auto *board_graph = new Graph(board);
+    size_t dimension = board_graph->GetDimension();
+    vector<Graph::Node *> nodes = board_graph->GetNodes();
 
-    REQUIRE(board_graph->GetDimension() == 3);
-    REQUIRE(board_graph->GetBoard() == board);
+    vector<vector<int>> adjacent = {{3, 1}, {4, 2, 0}, {5, 1}, {6, 4, 0},
+                                    {7, 5, 3, 1}, {8, 4, 2}, {7, 3}, {8, 6, 4},
+                                    {7, 5}};
 
-    vector<int> adjacent = {3, 1};
-    REQUIRE(board_graph->GetNodes().at(0)->row == 0);
-    REQUIRE(board_graph->GetNodes().at(0)->col == 0);
-    REQUIRE(board_graph->GetNodes().at(0)->adjacent == adjacent);
-    REQUIRE(board_graph->GetNodes().at(0)->distance == INFINITY);
-
-    adjacent = {4, 2, 0};
-    REQUIRE(board_graph->GetNodes().at(1)->row == 0);
-    REQUIRE(board_graph->GetNodes().at(1)->col == 1);
-    REQUIRE(board_graph->GetNodes().at(1)->adjacent == adjacent);
-    REQUIRE(board_graph->GetNodes().at(1)->distance == INFINITY);
-
-    adjacent = {5, 1};
-    REQUIRE(board_graph->GetNodes().at(2)->row == 0);
-    REQUIRE(board_graph->GetNodes().at(2)->col == 2);
-    REQUIRE(board_graph->GetNodes().at(2)->adjacent == adjacent);
-    REQUIRE(board_graph->GetNodes().at(2)->distance == INFINITY);
-
-    adjacent = {6, 4, 0};
-    REQUIRE(board_graph->GetNodes().at(3)->row == 1);
-    REQUIRE(board_graph->GetNodes().at(3)->col == 0);
-    REQUIRE(board_graph->GetNodes().at(3)->adjacent == adjacent);
-    REQUIRE(board_graph->GetNodes().at(3)->distance == INFINITY);
-
-    adjacent = {7, 5, 3, 1};
-    REQUIRE(board_graph->GetNodes().at(4)->row == 1);
-    REQUIRE(board_graph->GetNodes().at(4)->col == 1);
-    REQUIRE(board_graph->GetNodes().at(4)->adjacent == adjacent);
-    REQUIRE(board_graph->GetNodes().at(4)->distance == INFINITY);
-
-    adjacent = {8, 4, 2};
-    REQUIRE(board_graph->GetNodes().at(5)->row == 1);
-    REQUIRE(board_graph->GetNodes().at(5)->col == 2);
-    REQUIRE(board_graph->GetNodes().at(5)->adjacent == adjacent);
-    REQUIRE(board_graph->GetNodes().at(5)->distance == INFINITY);
-
-    adjacent = {7, 3};
-    REQUIRE(board_graph->GetNodes().at(6)->row == 2);
-    REQUIRE(board_graph->GetNodes().at(6)->col == 0);
-    REQUIRE(board_graph->GetNodes().at(6)->adjacent == adjacent);
-    REQUIRE(board_graph->GetNodes().at(6)->distance == INFINITY);
-
-    adjacent = {8, 6, 4};
-    REQUIRE(board_graph->GetNodes().at(7)->row == 2);
-    REQUIRE(board_graph->GetNodes().at(7)->col == 1);
-    REQUIRE(board_graph->GetNodes().at(7)->adjacent == adjacent);
-    REQUIRE(board_graph->GetNodes().at(7)->distance == INFINITY);
-
-    adjacent = {7, 5};
-    REQUIRE(board_graph->GetNodes().at(8)->row == 2);
-    REQUIRE(board_graph->GetNodes().at(8)->col == 2);
-    REQUIRE(board_graph->GetNodes().at(8)->adjacent == adjacent);
-    REQUIRE(board_graph->GetNodes().at(8)->distance == INFINITY);
+    for (size_t i = 0; i < dimension; i++) {
+      REQUIRE(nodes[i]->row == (i / dimension));
+      REQUIRE(nodes[i]->col == (i % dimension));
+      REQUIRE(nodes[i]->distance == INFINITY);
+      REQUIRE(nodes[i]->adjacent == adjacent[i]);
+    }
   }
 
   SECTION("Testing the Constructor with a 2x2 board with walls") {
@@ -114,33 +54,17 @@ TEST_CASE("Testing the Graph class") {
                                  {1, 0}};
 
     auto *board_graph = new Graph(board);
+    size_t dimension = board_graph->GetDimension();
+    vector<Graph::Node *> nodes = board_graph->GetNodes();
 
-    REQUIRE(board_graph->GetDimension() == 2);
-    REQUIRE(board_graph->GetBoard() == board);
+    vector<vector<int>> adjacent = {{1}, {3, 0}, {}, {1}};
 
-    vector<int> adjacent = {1};
-    REQUIRE(board_graph->GetNodes().at(0)->row == 0);
-    REQUIRE(board_graph->GetNodes().at(0)->col == 0);
-    REQUIRE(board_graph->GetNodes().at(0)->adjacent == adjacent);
-    REQUIRE(board_graph->GetNodes().at(0)->distance == INFINITY);
-
-    adjacent = {3, 0};
-    REQUIRE(board_graph->GetNodes().at(1)->row == 0);
-    REQUIRE(board_graph->GetNodes().at(1)->col == 1);
-    REQUIRE(board_graph->GetNodes().at(1)->adjacent == adjacent);
-    REQUIRE(board_graph->GetNodes().at(1)->distance == INFINITY);
-
-    adjacent = {};
-    REQUIRE(board_graph->GetNodes().at(2)->row == 1);
-    REQUIRE(board_graph->GetNodes().at(2)->col == 0);
-    REQUIRE(board_graph->GetNodes().at(2)->adjacent == adjacent);
-    REQUIRE(board_graph->GetNodes().at(2)->distance == INFINITY);
-
-    adjacent = {1};
-    REQUIRE(board_graph->GetNodes().at(3)->row == 1);
-    REQUIRE(board_graph->GetNodes().at(3)->col == 1);
-    REQUIRE(board_graph->GetNodes().at(3)->adjacent == adjacent);
-    REQUIRE(board_graph->GetNodes().at(3)->distance == INFINITY);
+    for (size_t i = 0; i < dimension; i++) {
+      REQUIRE(nodes[i]->row == (i / dimension));
+      REQUIRE(nodes[i]->col == (i % dimension));
+      REQUIRE(nodes[i]->distance == INFINITY);
+      REQUIRE(nodes[i]->adjacent == adjacent[i]);
+    }
   }
 
   SECTION("Testing the Constructor with a 3x3 board with walls") {
@@ -149,63 +73,18 @@ TEST_CASE("Testing the Graph class") {
                                  {0, 1, 1}};
 
     auto *board_graph = new Graph(board);
+    size_t dimension = board_graph->GetDimension();
+    vector<Graph::Node *> nodes = board_graph->GetNodes();
 
-    REQUIRE(board_graph->GetDimension() == 3);
-    REQUIRE(board_graph->GetBoard() == board);
+    vector<vector<int>> adjacent = {{3, 1}, {4, 2, 0}, {1}, {6, 4, 0}, {3, 1},
+                                    {}, {3}, {}, {}};
 
-    vector<int> adjacent = {3, 1};
-    REQUIRE(board_graph->GetNodes().at(0)->row == 0);
-    REQUIRE(board_graph->GetNodes().at(0)->col == 0);
-    REQUIRE(board_graph->GetNodes().at(0)->adjacent == adjacent);
-    REQUIRE(board_graph->GetNodes().at(0)->distance == INFINITY);
-
-    adjacent = {4, 2, 0};
-    REQUIRE(board_graph->GetNodes().at(1)->row == 0);
-    REQUIRE(board_graph->GetNodes().at(1)->col == 1);
-    REQUIRE(board_graph->GetNodes().at(1)->adjacent == adjacent);
-    REQUIRE(board_graph->GetNodes().at(1)->distance == INFINITY);
-
-    adjacent = {1};
-    REQUIRE(board_graph->GetNodes().at(2)->row == 0);
-    REQUIRE(board_graph->GetNodes().at(2)->col == 2);
-    REQUIRE(board_graph->GetNodes().at(2)->adjacent == adjacent);
-    REQUIRE(board_graph->GetNodes().at(2)->distance == INFINITY);
-
-    adjacent = {6, 4, 0};
-    REQUIRE(board_graph->GetNodes().at(3)->row == 1);
-    REQUIRE(board_graph->GetNodes().at(3)->col == 0);
-    REQUIRE(board_graph->GetNodes().at(3)->adjacent == adjacent);
-    REQUIRE(board_graph->GetNodes().at(3)->distance == INFINITY);
-
-    adjacent = {3, 1};
-    REQUIRE(board_graph->GetNodes().at(4)->row == 1);
-    REQUIRE(board_graph->GetNodes().at(4)->col == 1);
-    REQUIRE(board_graph->GetNodes().at(4)->adjacent == adjacent);
-    REQUIRE(board_graph->GetNodes().at(4)->distance == INFINITY);
-
-    adjacent = {};
-    REQUIRE(board_graph->GetNodes().at(5)->row == 1);
-    REQUIRE(board_graph->GetNodes().at(5)->col == 2);
-    REQUIRE(board_graph->GetNodes().at(5)->adjacent == adjacent);
-    REQUIRE(board_graph->GetNodes().at(5)->distance == INFINITY);
-
-    adjacent = {3};
-    REQUIRE(board_graph->GetNodes().at(6)->row == 2);
-    REQUIRE(board_graph->GetNodes().at(6)->col == 0);
-    REQUIRE(board_graph->GetNodes().at(6)->adjacent == adjacent);
-    REQUIRE(board_graph->GetNodes().at(6)->distance == INFINITY);
-
-    adjacent = {};
-    REQUIRE(board_graph->GetNodes().at(7)->row == 2);
-    REQUIRE(board_graph->GetNodes().at(7)->col == 1);
-    REQUIRE(board_graph->GetNodes().at(7)->adjacent == adjacent);
-    REQUIRE(board_graph->GetNodes().at(7)->distance == INFINITY);
-
-    adjacent = {};
-    REQUIRE(board_graph->GetNodes().at(8)->row == 2);
-    REQUIRE(board_graph->GetNodes().at(8)->col == 2);
-    REQUIRE(board_graph->GetNodes().at(8)->adjacent == adjacent);
-    REQUIRE(board_graph->GetNodes().at(8)->distance == INFINITY);
+    for (size_t i = 0; i < dimension; i++) {
+      REQUIRE(nodes[i]->row == (i / dimension));
+      REQUIRE(nodes[i]->col == (i % dimension));
+      REQUIRE(nodes[i]->distance == INFINITY);
+      REQUIRE(nodes[i]->adjacent == adjacent[i]);
+    }
   }
 
   SECTION("Testing the Constructor with a 3x3 board with walls and an"
@@ -216,15 +95,16 @@ TEST_CASE("Testing the Graph class") {
 
     auto *board_graph = new Graph(board);
     size_t dimension = board_graph->GetDimension();
+    vector<Graph::Node *> nodes = board_graph->GetNodes();
 
     vector<vector<int>> adjacent = {{3, 1}, {2, 0}, {1}, {6, 0}, {}, {}, {3},
                                     {}, {}};
 
     for (size_t i = 0; i < dimension; i++) {
-      REQUIRE(board_graph->GetNodes()[i]->row == (i / dimension));
-      REQUIRE(board_graph->GetNodes()[i]->col == (i % dimension));
-      REQUIRE(board_graph->GetNodes()[i]->distance == INFINITY);
-      REQUIRE(board_graph->GetNodes()[i]->adjacent == adjacent[i]);
+      REQUIRE(nodes[i]->row == (i / dimension));
+      REQUIRE(nodes[i]->col == (i % dimension));
+      REQUIRE(nodes[i]->distance == INFINITY);
+      REQUIRE(nodes[i]->adjacent == adjacent[i]);
     }
   }
 }
